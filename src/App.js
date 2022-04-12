b2621a64b4cb6fa8a0e6e7ff7b014aaade2bd7cc
Lsy322 <j3a2s2o2n8@gmail.com>
2022-04-12 22:09:45 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -42,7 +42,7 @@ function App() {
   useEffect(() => {
     if (token != null) {
       setSocket(
-        io("https://csci3100takeiteasy.herokuapp.com:8080", {
+        io("https://csci3100takeiteasy.herokuapp.com", {
           query: { token },
         })
       );
